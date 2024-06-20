6d49014369280daa2794cddd2a7205f5474ffa83
Ojie Precious <peterjohn56464@gmail.com>
2024-06-21 00:15:51 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,28 @@ void main() {
           backgroundColor: Colors.blue,
           title: const Text(
             'My App',
+            style: TextStyle(color: Colors.white),
           ),
           centerTitle: true,
         ),
-        body: Center(
-          child: Text('Hello Ninjas'),
+        body: const Center(
+          child: Text(
+            'Hello Ninjas',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.grey,
+                fontFamily: 'IndieFlower'),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.blue,
+          child: const Text(
+            'click',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
     ),
