59d752d9171ec7219e9283d3e32a9610869e9dcd
Ojie Precious <peterjohn56464@gmail.com>
2024-06-21 08:22:37 +0100

    icons

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text(
-            'My App',
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text(
-            'Hello Ninjas',
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Colors.grey,
-                fontFamily: 'IndieFlower'),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.blue,
-          child: const Text(
-            'click',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-      ),
+    const MaterialApp(
+      home: Home(),
     ),
   );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          'My App',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.orange,
+          size: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blue,
+        child: const Text(
+          'click',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
 }
