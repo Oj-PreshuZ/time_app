32838321b6f86850665250151d8afb4812cdd86c
Ojie Precious <peterjohn56464@gmail.com>
2024-06-25 15:55:06 +0100

    states

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:world_timeapp/pages/choose_location.dart';
 import 'package:world_timeapp/pages/home.dart';
+import 'package:world_timeapp/pages/loading.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Home(),
+    MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => const Loading(),
+        '/home': (context) => const Home(),
+        '/location': (context) => const ChooseLocation(),
+      },
     ),
   );
 }
